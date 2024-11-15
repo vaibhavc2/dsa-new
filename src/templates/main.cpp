93b1c812bca60e4cd2e038c4522b2e1ae0c7308e
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-11-15 15:30:14 +0530

    refactor templates

--- src/templates/main.cpp
+++ src/templates/main.cpp
@@ -23,6 +23,83 @@ using namespace std;
     cout.precision(db_precision); \
     cout << fixed
 
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T, class V>
+void _print(unordered_map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <typename T>
+void _print(T x);
+
+// function implementations
+template <typename T>
+void _print(T x) { cerr << x; }
+
+template <class T, class V>
+void _print(pair<T, V> p) {
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+
+template <class T>
+void _print(vector<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(set<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(multiset<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V>
+void _print(map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V>
+void _print(unordered_map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
 
 inline void input(int &n) {
     cin >> n;
