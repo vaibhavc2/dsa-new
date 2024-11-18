e6557aa6d2214d80fc8759f819cf3b1654db452b
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-11-18 22:08:02 +0530

    solve questions, clear basics

---
+++ src/leetcode/121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,223 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,abm,bmi,bmi2,mmx,avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+// #define int long long
+#define fastIO()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+// const int MOD = 1e9+7;
+// const int _MOD = 998244353;
+// const double EPS = 1e-9;
+// const long long INF = 2e18;
+// const int mxN = 2e5+5;
+// const double PI = 4 * atan(1);
+
+#define db_precision numeric_limits<double>::digits10 + 1
+#define DOUBLE()                  \
+    cout.precision(db_precision); \
+    cout << fixed
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T, class V>
+void _print(unordered_map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <typename T>
+void _print(T x);
+
+// function implementations
+template <typename T>
+void _print(T x) { cerr << x; }
+
+template <class T, class V>
+void _print(pair<T, V> p) {
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+
+template <class T>
+void _print(vector<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(set<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(multiset<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V>
+void _print(map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V>
+void _print(unordered_map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+inline void input(int &n) {
+    cin >> n;
+}
+
+inline void string_input(string &s) {
+    getline(cin, s);
+}
+
+inline void array_input(vector<int32_t> &v) {
+    for (int32_t i = 0; i < static_cast<int32_t>(v.size()); i++)
+        cin >> v[i];
+}
+
+inline void string_array_input(vector<string> &v) {
+    for (int32_t i = 0; i < static_cast<int32_t>(v.size()); i++)
+        getline(cin, v[i]);
+}
+
+// !! xxxxxxxx !! START FROM HERE !! xxxxxxxx !!
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            } else if (prices[i] - buyPrice > profit) {
+                profit = prices[i] - buyPrice;
+            }
+        }
+
+        return profit;        
+    }
+};
+// @lc code=end
+
+// Unoptimized solution - Brute force
+// Time complexity: O(n^2) -> TLE
+// Space complexity: O(1)
+class Solution1 {
+   public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        int i = 0, j = 0;
+        int profit = 0, diff = 0;
+
+        for (i = 0; i < n - 1; ++i) {
+            for (j = i + 1; j < n; ++j) {
+                if (prices[i] >= prices[j]) continue;
+                diff = prices[j] - prices[i];
+                if (profit < diff) profit = diff;
+            }
+        }
+
+        return profit;
+    }
+};
+
+// Optimized solution - Kadane's Algorithm
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0]; // buy price initialized with first day price
+        int profit = 0; // profit initialized with 0
+
+        for (int i = 1; i < prices.size(); i++) {
+            // if current price is less than buy price, update buy price
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            } else if (prices[i] - buyPrice > profit) {
+                // if current profit is greater than previous profit, update profit
+                profit = prices[i] - buyPrice;
+            }
+        }
+
+        return profit;        
+    }
+};
+
+inline void solve() {
+    int n;
+    input(n);
+
+    // string s;
+    // string_input(s);
+
+    vector<int32_t> v(n);
+    array_input(v);
+
+    // vector<string> vs(n);
+    // string_array_input(vs);
+
+    Solution sol;
+    auto ans = sol.maxProfit(v);
+    cout << ans << endl;
+}
+
+int32_t main() {
+    fastIO();
+    DOUBLE();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
---
+++ src/leetcode/7.reverse-integer.cpp
@@ -0,0 +1,216 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,abm,bmi,bmi2,mmx,avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+// #define int long long
+#define fastIO()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+// const int MOD = 1e9+7;
+// const int _MOD = 998244353;
+// const double EPS = 1e-9;
+// const long long INF = 2e18;
+// const int mxN = 2e5+5;
+// const double PI = 4 * atan(1);
+
+#define db_precision numeric_limits<double>::digits10 + 1
+#define DOUBLE()                  \
+    cout.precision(db_precision); \
+    cout << fixed
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T, class V>
+void _print(unordered_map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <typename T>
+void _print(T x);
+
+// function implementations
+template <typename T>
+void _print(T x) { cerr << x; }
+
+template <class T, class V>
+void _print(pair<T, V> p) {
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+
+template <class T>
+void _print(vector<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(set<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(multiset<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V>
+void _print(map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V>
+void _print(unordered_map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+inline void input(int &n) {
+    cin >> n;
+}
+
+inline void string_input(string &s) {
+    getline(cin, s);
+}
+
+inline void array_input(vector<int32_t> &v) {
+    for (int32_t i = 0; i < static_cast<int32_t>(v.size()); i++)
+        cin >> v[i];
+}
+
+inline void string_array_input(vector<string> &v) {
+    for (int32_t i = 0; i < static_cast<int32_t>(v.size()); i++)
+        getline(cin, v[i]);
+}
+
+// !! xxxxxxxx !! START FROM HERE !! xxxxxxxx !!
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    int reverse(int x) {
+        int res = 0;
+
+        while (x) {
+            if (res > INT_MAX / 10 || res < INT_MIN / 10) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
+// Iterative solution - 1
+// Time complexity: O(log(n))
+// Space complexity: O(1)
+class Solution1 {
+   public:
+    int reverse(int &x) {
+        int16_t sign = 1;
+        if (x < 0) sign = -1;
+
+        int num = abs(x);
+        int res = 0, digit = 0;
+
+        while (num > 0) {
+            digit = num % 10;
+            if (res > (INT_MAX - digit) / 10) return 0;
+            res = res * 10 + digit;
+            num /= 10;
+        }
+
+        return sign * res;
+    }
+};
+
+// Iterative solution - 2
+// Time complexity: O(log(n))
+// Space complexity: O(1)
+class Solution2 {
+   public:
+    int reverse(int x) {
+        int res = 0;
+
+        while (x) {
+            if (res > INT_MAX / 10 || res < INT_MIN / 10) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+
+        return res;
+    }
+};
+
+inline void solve() {
+    int n;
+    input(n);
+
+    // string s;
+    // string_input(s);
+
+    // vector<int32_t> v(n);
+    // array_input(v);
+
+    // vector<string> vs(n);
+    // string_array_input(vs);
+
+    Solution sol;
+    auto ans = sol.reverse(n);
+    cout << ans << endl;
+}
+
+int32_t main() {
+    fastIO();
+    DOUBLE();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
