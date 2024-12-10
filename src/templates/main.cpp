399e741d1189abafe26c2e3a82a263099dd8012d
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-12-10 23:33:29 +0530

    update template, gitignore, and settings/config

--- src/templates/main.cpp
+++ src/templates/main.cpp
@@ -7,186 +7,436 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endl "\n"
-/* Prefer 'using ll' in leetcode */
-// using ll = long long;
-// #define int long long
+#define endl '\n'
+#define cerr cerr << "ERROR :: "
 
 #define fastIO()                      \
     ios_base::sync_with_stdio(false); \
     cin.tie(nullptr);                 \
     cout.tie(nullptr)
 
+#define db_precision numeric_limits<double>::digits10 + 1
+#define DOUBLE()                  \
+    cout.precision(db_precision); \
+    cout << fixed
+
+// ************* IO Implementations *************
+
+template <typename T>
+inline void _print(const T &x) {
+    cout << x;
+}
+
+template <typename T, typename V>
+inline void _print(const pair<T, V> &p) {
+    cout << "{ ";
+    _print(p.first);
+    cout << ", ";
+    _print(p.second);
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(const vector<T> &v) {
+    cout << "{ ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        _print(v[i]);
+        if (i + 1 != v.size()) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(const deque<T> &d) {
+    cout << "{ ";
+    for (size_t i = 0; i < d.size(); ++i) {
+        _print(d[i]);
+        if (i + 1 != d.size()) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(const list<T> &l) {
+    cout << "{ ";
+    for (auto it = l.begin(); it != l.end(); ++it) {
+        _print(*it);
+        if (next(it) != l.end()) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(const set<T> &s) {
+    cout << "{ ";
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        _print(*it);
+        if (next(it) != s.end()) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(const unordered_set<T> &s) {
+    cout << "{ ";
+    bool first = true;
+    for (const auto &x : s) {
+        if (!first) cout << ", ";
+        _print(x);
+        first = false;
+    }
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(const multiset<T> &ms) {
+    cout << "{ ";
+    for (auto it = ms.begin(); it != ms.end(); ++it) {
+        _print(*it);
+        if (next(it) != ms.end()) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T, typename V>
+inline void _print(const map<T, V> &m) {
+    cout << "{ ";
+    for (auto it = m.begin(); it != m.end(); ++it) {
+        _print(*it);
+        if (next(it) != m.end()) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T, typename V>
+inline void _print(const unordered_map<T, V> &m) {
+    cout << "{ ";
+    bool first = true;
+    for (const auto p : m) {
+        if (!first) cout << ", ";
+        _print(p);
+        first = false;
+    }
+    cout << " }";
+}
+
+template <typename T>
+inline void _print(stack<T> s) {
+    vector<T> temp;
+    while (!s.empty()) {
+        temp.push_back(s.top());
+        s.pop();
+    }
+    reverse(temp.begin(), temp.end());
+    _print(temp);
+}
+
+template <typename T>
+inline void _print(queue<T> q) {
+    vector<T> temp;
+    while (!q.empty()) {
+        temp.push_back(q.front());
+        q.pop();
+    }
+    _print(temp);
+}
+
+template <typename T>
+inline void _print(priority_queue<T> pq) {
+    vector<T> temp;
+    while (!pq.empty()) {
+        temp.push_back(pq.top());
+        pq.pop();
+    }
+    sort(temp.rbegin(), temp.rend());
+    _print(temp);
+}
+
+template <typename T, size_t N>
+inline void _print(const array<T, N> &arr) {
+    cout << "{ ";
+    for (size_t i = 0; i < N; ++i) {
+        _print(arr[i]);
+        if (i + 1 != N) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename T, size_t N>
+inline void _print(const T (&arr)[N]) {
+    cout << "{ ";
+    for (size_t i = 0; i < N; ++i) {
+        _print(arr[i]);
+        if (i + 1 != N) cout << ", ";
+    }
+    cout << " }";
+}
+
+template <typename... Args>
+inline void _print(const tuple<Args...> &t) {
+    cout << "( ";
+    apply([](const auto &...args) { ((cout << args << ", "), ...); }, t);
+    cout.seekp(-2, ios_base::cur);
+    cout << " )";
+}
+
+template <typename T>
+inline void print(const T x) {
+    _print(x);
+    cout << endl;
+}
+
+template <typename T, typename... Args>
+inline void print(const T x, const Args ...args) {
+    _print(x);
+    cout << endl;
+    print(args...);
+}
+
+enum InputMethod {
+    Standard,
+    Line
+};
+
+template <typename T>
+inline void _input(T &x, InputMethod method = Standard) {
+    if constexpr (is_same<T, string>::value) {
+        if (method == Line) {
+            cin.ignore(); // ignores a new line ('\n')
+            cin.ignore(); // ignores a new line ('\n')
+            getline(cin, x);
+        } else {
+            cin >> x;
+        }
+    } else {
+        cin >> x;
+    }
+}
+
+template <typename T, size_t N>
+inline void _input(array<T, N> &arr, InputMethod method = Standard) {
+    for (auto &x : arr) {
+        _input(x, method);
+    }
+}
+
+template <typename T>
+inline void _input(vector<T> &v, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    v.resize(n);
+    for (auto &x : v) {
+        _input(x, method);
+    }
+}
+
+template <typename T>
+inline void _input(set<T> &s, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        T x;
+        _input(x, method);
+        s.insert(x);
+    }
+}
+
+template <typename T>
+inline void _input(unordered_set<T> &s, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        T x;
+        _input(x, method);
+        s.insert(x);
+    }
+}
+
+template <typename T>
+inline void _input(multiset<T> &s, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        T x;
+        _input(x, method);
+        s.insert(x);
+    }
+}
+
+template <typename T, typename V>
+inline void _input(map<T, V> &m, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        pair<T, V> p;
+        _input(p, method);
+        m.insert(p);
+    }
+}
+
+template <typename T, typename V>
+inline void _input(unordered_map<T, V> &m, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        pair<T, V> p;
+        _input(p, method);
+        m.insert(p);
+    }
+}
+
+template <typename T>
+inline void _input(list<T> &l, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    l.clear();
+    for (int i = 0; i < n; ++i) {
+        T x;
+        _input(x, method);
+        l.push_back(x);
+    }
+}
+
+template <typename T>
+inline void _input(queue<T> &q, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    while (!q.empty()) q.pop();
+    for (int i = 0; i < n; ++i) {
+        T x;
+        _input(x, method);
+        q.push(x);
+    }
+}
+
+template <typename T>
+inline void _input(deque<T> &d, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    d.resize(n);
+    for (auto &x : d) {
+        _input(x, method);
+    }
+}
+
+template <typename T>
+inline void _input(stack<T> &s, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    vector<T> temp(n);
+    for (int i = 0; i < n; ++i) {
+        _input(temp[i], method);
+    }
+    reverse(temp.begin(), temp.end());
+    while (!s.empty()) s.pop();
+    for (auto &x : temp) {
+        s.push(x);
+    }
+}
+
+template <typename T>
+inline void _input(priority_queue<T> &pq, InputMethod method = Standard) {
+    int n;
+    cin >> n;
+    while (!pq.empty()) pq.pop();
+    for (int i = 0; i < n; ++i) {
+        T x;
+        _input(x, method);
+        pq.push(x);
+    }
+}
+
+template <typename... Args>
+inline void _input(tuple<Args...> &t, InputMethod method = Standard) {
+    apply([method](auto &&...args) { ((_input(args, method)), ...); }, t);
+}
+
+template <typename T>
+inline void _input(T *arr, size_t n, InputMethod method = Standard) {
+    for (size_t i = 0; i < n; ++i) {
+        _input(arr[i], method);
+    }
+}
+
+template <typename T, size_t N>
+inline void _input(T (&arr)[N]) {
+    for (size_t i = 0; i < N; ++i) {
+        cin >> arr[i];
+    }
+}
+
+template <typename... Args>
+inline void input(InputMethod method, Args &...args) {
+    (_input(args, method), ...);
+}
+
+// !! xxxxxxxx !! START FROM HERE !! xxxxxxxx !!
+
+//*/*-------------- SOLUTION --------------*/*//
+
+// @lc code=start
+
+// int a[10000] = {0};
 // const int MOD = 1e9+7;
 // const int _MOD = 998244353;
 // const double EPS = 1e-9;
 // const long long INF = 2e18;
 // const int mxN = 2e5+5;
 // const double PI = 4 * atan(1);
+// using ll = long long;
 
-#define db_precision numeric_limits<double>::digits10 + 1
-#define DOUBLE()                  \
-    cout.precision(db_precision); \
-    cout << fixed
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T, class V>
-void _print(unordered_map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <typename T>
-void _print(T x);
-
-// function implementations
-template <typename T>
-void _print(T x) { cerr << x; }
-
-template <class T, class V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.first);
-    cerr << ",";
-    _print(p.second);
-    cerr << "}";
-}
-
-template <class T>
-void _print(vector<T> v) {
-    cerr << "[ ";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-template <class T>
-void _print(set<T> v) {
-    cerr << "[ ";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-template <class T>
-void _print(multiset<T> v) {
-    cerr << "[ ";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-template <class T, class V>
-void _print(map<T, V> v) {
-    cerr << "[ ";
-    for (auto i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-template <class T, class V>
-void _print(unordered_map<T, V> v) {
-    cerr << "[ ";
-    for (auto i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-inline void input(int &n) {
-    cin >> n;
-}
-
-inline void string_input(string &s) {
-    getline(cin, s);
-}
-
-inline void vector_input(vector<int32_t> &v) {
-    for (int32_t i = 0; i < static_cast<int32_t>(v.size()); i++)
-        cin >> v[i];
-}
-
-inline void string_vector_input(vector<string> &v) {
-    for (int32_t i = 0; i < static_cast<int32_t>(v.size()); i++)
-        getline(cin, v[i]);
-}
-
-inline void array_input(int32_t arr[], int32_t n) {
-    for (int32_t i = 0; i < n; i++)
-        cin >> arr[i];
-}
-
-inline void string_array_input(string arr[], int32_t n) {
-    for (int32_t i = 0; i < n; i++)
-        getline(cin, arr[i]);
-}
-
-inline void char_array_input(char arr[], int32_t n) {
-    for (int32_t i = 0; i < n; i++)
-        cin >> arr[i];
-}
-
-//*/*-------------- SOLUTION --------------*/*//
-// !! xxxxxxxx !! START FROM HERE !! xxxxxxxx !!
-// @lc code=start
 class Solution {
    public:
     int func(int n) {
         return n;
     }
 };
+
 // @lc code=end
 
-
 //*/*-------------- SOLUTIONS --------------*/*//
 
 //*/*-------------- SOLUTIONS --------------*/*//
 
+void solve() {
+    int x;
+    string s;
+    vector<int> v;
+    vector<vector<int>> vv;
 
-inline void solve() {
-    int n;
-    input(n);
-
-    // string s;
-    // cin >> s;
-    // string_input(s);
-
-    // vector<int32_t> v(n);
-    // vector_input(v);
-
-    // int32_t arr[10000];
-    // array_input(arr, n);
+    input(Standard, x, v, vv);
+    input(Line, s);
 
     Solution sol;
-    auto ans = sol.func(n);
-    cout << ans << endl;
+    auto ans = sol.func(x);
+    print(ans);
 }
 
 int32_t main() {
 #ifndef DEBUG
     fastIO();
-    DOUBLE();
 #endif
+    DOUBLE();
 
-    int t = 1;
+    int t = 1, n;
     cin >> t;
+    n = t;
 
-    while (t--)
-        solve();
+    // while (t--) solve();
+
+    const int timeout_seconds = 5;  // in seconds
+
+    while (t--) {
+        auto future = async(launch::async, solve);  // Run solve() in a separate thread
+
+        if (future.wait_for(chrono::seconds(timeout_seconds)) == future_status::timeout) {
+            cerr << "Time Limit Exceeded at 'Test case: " << n - t << "'\n";
+
+            exit(0);  // exit the program
+        }
+    }
 
     return 0;
 }
+
+// ********************** END *********************
